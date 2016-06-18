f47f4a3bb4813a9ad6fc90c85b08220763281ad9
Alexey Veretennikov <alexey.veretennikov@gmail.com>
2016-06-18 13:51:48 +0200

    Implemented parser of index files

--- git-pack.lisp
+++ git-pack.lisp
@@ -1,10 +1,23 @@
 ;;;; git-pack.lisp
+;;
+;; This package reads the git pack and index files
+;; according to the official documentation:
+;; https://www.kernel.org/pub/software/scm/git/docs/technical/pack-format.txt
+;;
+;; See also this blog entry:
+;; https://codewords.recurse.com/issues/three/unpacking-git-packfiles/
+;;
+;; To understand how the pack file is constructed also helps to
+;; read the source: https://github.com/git/git/blob/master/pack-write.c
+;;
 (defpackage #:gitplot.git-pack
   (:use #:cl #:cl-annot.class #:alexandria #:gitplot.utils)
   (:export main))
 
 (in-package #:gitplot.git-pack)
 (annot:enable-annot-syntax)
+
+;; 
 
 ;;----------------------------------------------------------------------------
 ;; Pack file format
@@ -29,8 +42,8 @@
 (defun read-pack-entry-header (stream)
   "Reads the current stream for git pack entry header.
 Pack entry header format (from the official documentation
-on https://www.kernel.org/pub/software/scm/git/docs/technical/pack-format.txt
-``
+on https://www.kernel.org/pub/software/scm/git/docs/technical/pack-format.txt)
+-------------------------------------------------------
 1-byte size extension bit (MSB)
        type (next 3 bit)
        size0 (lower 4-bit)
@@ -38,7 +51,7 @@ n-byte sizeN (as long as MSB is set, each 7-bit)
         size0..sizeN form 4+7+7+..+7 bit integer, size0
 	is the least significant part, and sizeN is the
 	most significant part.
-``
+-------------------------------------------------------
 Example: encoded entry of type 1 and of size 7877.
 Binary representation: 1 1110 1100 0101
 Let's group it  by 7 7 4 bits
@@ -65,7 +78,8 @@ Remove msb -> 108, shift << 4 = 1728.
 1728 + 5(extracted from 1st byte) = 1733
 Third byte, no msb -> 3, shift 11 (4 + 7) = 6144
 And finally the length is 6144 + 1733 = 7833
-The function returns (type . len) cons.
+The function returns (TYPE . LEN) cons.
+LEN here is the the size of unpacked data.
 "
   (declare (optimize (float 0)))
   (let* ((head (read-byte stream))
@@ -80,3 +94,103 @@ The function returns (type . len) cons.
             (incf shift 7)))
     (cons type len)))
                               
+;;----------------------------------------------------------------------------
+;; Index file format
+;;----------------------------------------------------------------------------
+(defun parse-index-file (filename)
+  (with-open-file (stream filename :direction :input :element-type '(unsigned-byte 8))
+    (let ((header (make-array 4
+                              :element-type '(unsigned-byte 8)
+                              :fill-pointer t)))
+      ;; 1. read the header 4 bytes
+      (read-sequence header stream :end 4)
+      ;; check header word
+      (when (and (equalp header #(255 116 79 99))
+                 ;; read the version 4 bytes and check version = 2
+                 (= 2 (nibbles:read-ub32/be stream)))
+        ;; 2. read the fanout table
+        (let* ((fanout (read-fanout-table stream))
+               (size (aref fanout 255))
+               ;; 3. read sorted list of object names
+               (object-names (read-object-names stream size))
+               (crcs (read-crcs stream size))
+               (offsets (read-offsets stream size)))
+          offsets)))))
+          
+
+(defun read-fanout-table (stream)
+  "Reads the fanout table for index file from stream.
+According to the documentation (https://www.kernel.org/pub/software/scm/git/docs/technical/pack-format.txt):
+----------------------------------------------------------
+256 4-byte network byte order
+integers.  N-th entry of this table records the number of
+objects in the corresponding pack, the first byte of whose
+object name is less than or equal to N.  
+----------------------------------------------------------
+One can deduct number of entries in pack file - it should be in the last
+element of the table (since any byte sequence starts with the number
+less or equal to 256, as the byte <= 256"
+  (let ((fanout (make-array 256 :element-type 'integer :fill-pointer t)))
+    (loop for i from 0 to 255
+          do
+          (setf (aref fanout i) (nibbles:read-ub32/be stream)))
+    fanout))
+
+
+(defun read-object-names (stream size)
+  "Read the SIZE 20-bytes SHA1 codes of objects stored in PACK file"
+  (let ((object-names (make-array size :element-type '(vector unsigned-byte 8))))
+    (loop for i from 0 below size do
+          (let ((hash (make-array 20 :element-type '(unsigned-byte 8))))
+            (read-sequence hash stream)
+            (setf (aref object-names i) hash)))
+    object-names))
+
+
+(defun read-crcs (stream size)
+  "Read SIZE CRCs codes from the stream.
+Retuns an array of size SIZE with elements 4-bytes arrays with CRC codes"
+  (let* ((crcs (make-array size :element-type '(vector unsigned-byte 8))))
+    (loop for i from 0 below size do
+          (let ((crc (make-array 4 :element-type '(unsigned-byte 8))))
+            (read-sequence crc stream)
+            (setf (aref crcs i) crc)))
+    crcs))
+          
+    
+(defun read-offsets (stream size)
+  "Read offset table[s] and return the array of offsets in PACK file"
+  (let* ((offsets (make-array size :element-type 'integer))
+         (big-offsets nil)
+         (bytes-size (* size 4))
+         (table (make-array bytes-size :element-type '(unsigned-byte 8))))
+    ;; we will read all the table into bytes array
+    (read-sequence table stream)
+    (loop for i from 0 below bytes-size by 4 do
+          ;; processing separately depending if the MSB is set on the first
+          ;; byte of encoded length
+          (if (>= (aref table i) 128)
+              ;; if set we clear the msb 
+              (progn
+                (setf (aref table i) (logand 127 (aref table i)))
+                ;; and push to the list of "big offsets" as a pair:
+                ;; index in original table and index in big offsets table
+                (push (cons (ash i -2) (nibbles:ub32ref/be table i)) big-offsets))
+              ;; otherwise just NTOHL the value into the offsets array
+              (setf (aref offsets (ash i -2)) (nibbles:ub32ref/be table i))))
+    ;; WARNING!
+    ;; the code below for large (> 2gb) files in pack files
+    ;; has not been tested!!!
+    ;; after processing of small offsets let's process big offsets
+    (when big-offsets
+      ;; read all values from the stream
+      (let ((big-offsets-table
+             (make-array (length big-offsets) :element-type 'integer)))
+        (dotimes (i (length big-offsets))
+          (setf (aref big-offsets-table i) (nibbles:read-ub64/be stream)))
+        ;; process the list of needed big offsets and update original
+        ;; offsets array
+        (dolist (x big-offsets)
+          (setf (aref offsets (car x))
+                (aref big-offsets-table (cdr x))))))
+    offsets))
